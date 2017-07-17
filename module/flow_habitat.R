6b4f0f8938431d3ca5ce16e1fd9f499927df73e4
thesadie <sgill@flowwest.com>
2017-07-17 09:31:17 -0700

    remove reactives now that tabs

--- module/flow_habitat.R
+++ module/flow_habitat.R
@@ -19,33 +19,26 @@ flow_habitatUI <- function(id) {
 flow_habitat <- function(input, output, session, river_section) {
   
   output$fp_thresh_text <- renderUI({
-    tags$h6(paste0('dashed gold line denotes floodplain threshold (', pretty_num(fp_hab()$threshold, 0), ' cfs)'))
+    tags$h6(paste0('dashed gold line denotes floodplain threshold (', pretty_num(fp_hab$threshold, 0), ' cfs)'))
   })
   
-  shed_flow <- reactive({
-    filter(sim_flow, shed == river_section)
-  })
+  shed_flow <- filter(sim_flow, shed == river_section)
   
-  shed_hab <- reactive({
-    filter(habitat, location == river_section)
-  })
+  shed_hab <- filter(habitat, location == river_section)
   
-  fp_hab <- reactive({
-    fp_lookup %>% 
-      filter(location == river_section)
-  })
+  fp_hab <- filter(fp_lookup, location == river_section)
   
   output$area <- renderPlotly({
 
-    xan <- fp_hab()$threshold + (max(shed_hab()$flow) - min(shed_hab()$flow)) * .1 
-    yan <- (max(shed_hab()$acres))/2
+    xan <- fp_hab$threshold + (max(shed_hab$flow) - min(shed_hab$flow)) * .1 
+    yan <- (max(shed_hab$acres))/2
 
-    plot_ly(data = shed_hab(), x = ~flow, y = ~acres, color = ~stage, type = 'scatter', mode = 'lines', colors = 'Dark2',
+    plot_ly(data = shed_hab, x = ~flow, y = ~acres, color = ~stage, type = 'scatter', mode = 'lines', colors = 'Dark2',
             hoverinfo = 'text', text = ~paste(stage, '<br>',pretty_num(flow), 'cfs<br>', pretty_num(acres), 'acres')) %>%
-      add_lines(x = c(fp_hab()$threshold, max(shed_hab()$flow)), y = max(shed_hab()$acres) + 10, inherit = FALSE, fill = 'tozeroy',
+      add_lines(x = c(fp_hab$threshold, max(shed_hab$flow)), y = max(shed_hab$acres) + 10, inherit = FALSE, fill = 'tozeroy',
                 line = list(color = 'rgba(189,189,189, 0.1)'), hoverinfo = 'none', name = 'floodplain activated') %>%
       layout(annotations = list(x = xan, y = yan, xref = "x", yref = "y", showarrow = FALSE, 
-                                text = paste('additional', fp_hab()$fp_hab_acres,'acres of <br>floodplain habitat available')),
+                                text = paste('additional', fp_hab$fp_hab_acres,'acres of <br>floodplain habitat available')),
              yaxis = list(title = 'available habitat (acres)'),
              xaxis = list(title = 'flow (cfs)')) %>%
       config(displayModeBar = FALSE)
@@ -53,11 +46,11 @@ flow_habitat <- function(input, output, session, river_section) {
   })
 
   output$flow <- renderPlotly({
-    p <- shed_flow() %>% 
+    p <- shed_flow %>% 
       ggplot(aes(x = fct_inorder(month.name[month]), y = flow, text = paste(pretty_num(flow), 'cfs'))) +
       geom_boxplot() +
       geom_jitter(width = .2, size = 1, alpha = .2, stroke = 0) +
-      geom_hline(yintercept = fp_hab()$threshold, linetype = 'dash', size = .3, color = 'darkgoldenrod3') +
+      geom_hline(yintercept = fp_hab$threshold, linetype = 'dash', size = .3, color = 'darkgoldenrod3') +
       theme_minimal() +
       labs(y = 'flow (cfs)', x = '')
     
